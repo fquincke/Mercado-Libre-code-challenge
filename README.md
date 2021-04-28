4ce50af30aeab69c9eef5da66315df4d4588eeb2
Francisco Quincke <fhquincke@gmail.com>
2021-04-28 01:34:48 -0300

    merge changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mercado-Libre-code-challenge
 Code challenge para entrevista en Mercado Libre
+
+Asumo que la diagonal que no se muestra en el ejemplo tambien se considera de un mutante (diagonal hacia abajo e izquierda)
+
+Valido que no hayan letras incorrectas en el array en validator
+Asumo que no pueden llegar strings de largos diferentes y valido que el tamaño de la matriz es NxM en todas sus filas y columnas o que no haya una fila en null
+Cuando falla alguna de estas validaciones se retorna un código 400
--- services/mutantServices.js
+++ services/mutantServices.js
@@ -1,3 +1,26 @@
 exports.verify = function(dna) {
     return false;
-}
+};
+
+checkNitrogenBase = function(dna, row, col) {
+    const nitrogenBase = dna[row][col];
+
+    const canGoDown = row+3 < dna.length;
+    const canGoRight = col+3 < dna[0].length;
+    const canGoDiagonalRight = row+3 < dna.length && col+3 < dna[row].length;
+    const canGoDiagonalLeft = row+3 < dna.length && col-3 < dna[row].length;
+
+    if(canGoRight && nitrogenBase === dna[row][col+1] && nitrogenBase === dna[row][col+2] && nitrogenBase === dna[row][col+3]) {
+        return true;
+    }
+    if(canGoDown && nitrogenBase === dna[row+1][col] && nitrogenBase === dna[row+2][col] && nitrogenBase === dna[row+3][col]) {
+        return true;
+    }
+    if(canGoDiagonalRight && nitrogenBase === dna[row+1][col+1] && nitrogenBase === dna[row+2][col+2] && nitrogenBase === dna[row+3][col+3]) {
+        return true;
+    }
+    if(canGoDiagonalLeft && nitrogenBase === dna[row+1][col-1] && nitrogenBase === dna[row+2][col-2] && nitrogenBase === dna[row+3][col-3]) {
+        return true;
+    }
+    return false;
+};
